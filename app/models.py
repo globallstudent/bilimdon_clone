d904602a300958fda5eb5e6d594c0fe37479d7dd
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-04-04 18:50:01 +0500

    new models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import String, DateTime, Date
+from sqlalchemy import String, DateTime, Date, ForeignKey, Integer
 
-from datetime import datetime, date, timezone
+from typing import Optional
+
+from datetime import datetime, date, timezone, time
 
 from app.database import Base
 
@@ -20,3 +22,38 @@ class User(Base):
     is_active: Mapped[bool] = mapped_column(default=True)
     is_staff: Mapped[bool] = mapped_column(default=False)
     is_superuser: Mapped[bool] = mapped_column(default=False)
+
+
+class Participation(Base):
+    __tablename__ = "particiaption"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        type_=Integer,
+        nullable=False
+    )
+    user: Mapped[Optional["User"]] = relationship("User")
+
+    game_id: Mapped[int] = mapped_column()
+    start_time: Mapped[time.time]
+    end_time: Mapped[Optional[time]] = mapped_column(nullable=False)
+    gained_score: Mapped[int] = mapped_column()
+    registered_at: Mapped[datetime]
+
+class Game(Base):
+    __tablename__ = "games"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        type_=Integer,
+        nullable=False
+    )
+    owner: Mapped[Optional["User"]] = relationship("User")
+    topic_id: Mapped[int] = mapped_column()
+    title: Mapped[String] = mapped_column()
+    description: Mapped[String] = mapped_column()
+    score: Mapped[int] = mapped_column()
+    start_time: Mapped[time]
+    end_time: Mapped[time]
