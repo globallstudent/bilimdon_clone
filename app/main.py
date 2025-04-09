2ee6b013340a6c5d009a6c1d0952c477669327eb
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-04-09 21:33:29 +0500

    add routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers import auth
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(auth.router)
